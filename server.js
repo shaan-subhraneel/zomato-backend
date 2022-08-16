b622fcff2aa698eaad5b3fd049e7dd799ac5dd44
Subhraneel Chowdhury <subhraneelchowdhury@Subhraneels-Air.domain.name>
2022-08-16 11:28:01 +0530

    update

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get('/userInfo',(req,res) => {
   // jwt verify token
   jwt.verify(token,config.secret,(err,user) => {
       if(err) return res.send({auth:false,token:'Invalid Token'})
-      db.collection('users').findById(user.id,(err,result) => {
+      db.collection('users').findById(user.id).toArray((err,result) => {
           res.send(result)
       })
   })
